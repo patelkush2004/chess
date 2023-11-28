c8c5d71607a005dcbaf77b5bedbaaf926e461546
Your Name <ali.snaqvi09@gmail.com>
2023-11-28 00:15:15 -0500

    nit(gitignore): init gitignore and rm cache .o, .d files

--- main.d
+++
@@ -1,2 +0,0 @@
-main.o: main.cc board.h piece.h player.h subject.h observer.h \
- textdisplay.h graphicdisplay.h window.h human.h
--- textdisplay.d
+++
@@ -1,2 +0,0 @@
-textdisplay.o: textdisplay.cc textdisplay.h observer.h board.h piece.h \
- player.h subject.h graphicdisplay.h window.h
