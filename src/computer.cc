aa40cdc291ac33876a786ece3e4d5f03a8daf230
smartahsan <maaasyed@uwaterloo.ca>
2023-12-04 03:34:22 -0500

    fix(computer): fixed levelOne, working now

--- src/computer.cc
+++ src/computer.cc
@@ -27,55 +27,41 @@ pair<int, int> Computer::convertToCoord(string notation) {
 
 vector<pair<int, int>> Computer::makeComputerMove(Board& b)  {
 
-    // do NOT ever use Piece by value, this causes seg faults. Use pointer to Piece instead.
-    vector<Piece*> availablePieces;
-    vector<pair<int, int>> moves;
     int randomIndex;
-    Piece *selectedPiece = nullptr;
-    
-    while (true) {
-        // finds all available team pieces 
-        // Nit: this should be handled by board or a vector in player!
-        for(int x = 0; x < 8; x++) {
-            for (int y = 0; y < 8; y++) {
-                if (b.getPiece(x, y)->getTeam() == this->getTeam() && !b.getPiece(x, y)->isBlank()) {
-                    availablePieces.emplace_back(b.getPiece(x, y));
-                }
+
+    // this is a vector of pairs of <currentPosition, vectorPossibleMoves>
+    vector<
+        pair<
+            pair<int, int>, // current
+            pair<int, int> // possibleMove
+        >
+    > moves;
+
+    for(int x = 0; x < 8; x++) {
+        for (int y = 0; y < 8; y++) {
+            if (b.getPiece(x, y)->getTeam() == this->getTeam() && !b.getPiece(x, y)->isBlank()) {
+
+                vector<pair<int, int>> possibleMoves =  b.getPiece(x, y)->calculatePossibleMoves();
+                pair<int, int> current(b.getPiece(x, y)->getRow(), b.getPiece(x, y)->getCol());
+
+                for (pair<int, int> move : possibleMoves) {
+                    pair<pair<int, int>, pair<int, int>> currentAndPossibleMove(current, move);
+                    moves.emplace_back(currentAndPossibleMove);
+                }        
             }
-        } // this is working perfectly, traverses through each row. 
-
-        // seed-safe random index generation for availabliePieces
-        randomIndex = randomGenerator(0, availablePieces.size() - 1);
-
-        selectedPiece = availablePieces[randomIndex];
-        //selectedPiece = b.getPiece(6,1);
-
-        moves = selectedPiece->calculatePossibleMoves();
-
-        // this checks if the piece has any possible moves, if not, it will clear the availablePieces vector and moves vector and try again
-        if (moves.size() == 0) {
-            availablePieces.clear();
-            moves.clear();
-            continue;
-        };
-
-        break;
+        }
     }
 
     // seed-safe random move generation for availabliePieces
     randomIndex = randomGenerator(0, moves.size() - 1);
 
-    pair<int, int> move = moves[randomIndex];
-    pair<int, int> current(selectedPiece->getRow(), selectedPiece->getCol());
+    pair<pair<int, int>, pair<int, int>> move = moves[randomIndex];
 
     vector<pair<int, int>> fromToMove;
 
-    fromToMove.emplace_back(current);
-    fromToMove.emplace_back(move);
+    fromToMove.emplace_back(move.first);
+    fromToMove.emplace_back(move.second);
 
     return fromToMove;
 }
 
-
-//vector<pair<int, int>> Computer::levelOne(Board& b) {
-//}
